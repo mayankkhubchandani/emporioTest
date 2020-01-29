3880200fc0ec090e20051fe2bbb3e510efef4d50
Mayank Khubchandani <mayankkhubchandani@gmail.com>
2020-01-29 14:26:54 +0800

    Updating log4j version to latest one

--- emporiofunctionaltest/src/main/java/com/emporio/pages/BasePage.java
+++ emporiofunctionaltest/src/main/java/com/emporio/pages/BasePage.java
@@ -2,11 +2,12 @@ package com.emporio.pages;
 
 import com.emporio.driver.ChromeBrowserDriver;
 import com.emporio.driver.CommonBrowserDriver;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 public class BasePage {
     protected static CommonBrowserDriver driver = new ChromeBrowserDriver();
-    protected static Logger logger = Logger.getLogger(HomePage.class);
+    protected static Logger logger = LogManager.getLogger(HomePage.class);
 
     public BasePage() {
     }
